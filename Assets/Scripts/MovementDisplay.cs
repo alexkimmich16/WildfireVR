e6f226f4d49141520ed94abf52e142389ed7d958
alexkimmich16 <alexkimmich16@gmail.com>
2022-03-22 14:35:37 -0400

    Laptop Commit

--- Assets/Scripts/MovementDisplay.cs
+++ Assets/Scripts/MovementDisplay.cs
@@ -9,6 +9,7 @@ public class MovementDisplay : MonoBehaviour
     private List<GameObject> Sides = new List<GameObject>(2);
     private int Current;
     private HandMagic HM;
+    public Vector3 Offset;
 
 
     public float FramesPerSecond;
@@ -19,15 +20,16 @@ public class MovementDisplay : MonoBehaviour
 
     public static float CastTime = 5f;
     private bool ShouldCountFrames = true;
+    [Range(0.1f, 5)]
+    public float Scale;
 
     private void Start()
     {
         ShouldCountFrames = true;
         Sides = new List<GameObject>(2);
         //0 is left
-        //hands:hands_geom
-        Sides.Add(transform.Find("LeftHand").gameObject);
-        Sides.Add(transform.Find("RightHand").gameObject);
+        Sides.Add(transform.GetChild(0).Find("LeftHand").gameObject);
+        Sides.Add(transform.GetChild(0).Find("RightHand").gameObject);
 
         HM = HandMagic.instance;
         for (int i = 0; i < Sides.Count; i++)
@@ -36,26 +38,31 @@ public class MovementDisplay : MonoBehaviour
     }
     void Update()
     {
-        CountTimer();
-        int move = (int)movement;
-        if (Current > HM.Spells[move].FinalInfo.LeftLocalPos.Count - 1)
-        {
-            CastSpell();
-        }
-
-        for (int i = 0; i < Sides.Count; i++)
-        {
-            Sides[i].transform.position = ConvertPoint(GetLocalPosSide(i, move, Current));
-            Sides[i].transform.eulerAngles = GetRotationSide(i, move, Current);
-        }
-
+        if (ShouldCountFrames == false)
+            return;
         
-        //then display frames    
+        int move = (int)movement;
+        Debug.Log("Count: " + HM.Spells[move].FinalInfo.LeftLocalPos.Count + "Current: " + Current);
+        if (Current > HM.Spells[move].FinalInfo.LeftLocalPos.Count - 2)
+            CastSpell();
+        else if(ShouldCountFrames == true)
+        {
+            CountTimer();
+
+            for (int i = 0; i < Sides.Count; i++)
+            {
+                Vector3 CurrentPos;
+                if (i == 0)
+                    CurrentPos = HandMagic.instance.Spells[move].FinalInfo.LeftLocalPos[Current];
+                else
+                    CurrentPos = HandMagic.instance.Spells[move].FinalInfo.RightLocalPos[Current];
+                Sides[i].transform.position = ConvertPoint(CurrentPos) + Offset;
+                Sides[i].transform.eulerAngles = GetRotationSide(i, move, Current);
+            }
+        }  
     }
     void CastSpell()
     {
-        Current = 0;
-        Timer = 0;
         ShouldCountFrames = false;
         if ((int)movement == 0)
         {
@@ -92,15 +99,17 @@ public class MovementDisplay : MonoBehaviour
     {
         yield return new WaitForSeconds(CastTime);
         Destroy(SpawnedObject);
+        Current = 0;
+        Timer = 0;
         ShouldCountFrames = true;
         //end cast
     }
 
     public Vector3 ConvertPoint(Vector3 Local)
     {
-        float Distance = Local.x;
+        float Distance = Local.x * Scale;
         float RotationOffset = Local.y;
-        float HorizonalOffset = Local.z;
+        float HorizonalOffset = Local.z * Scale;
         Quaternion rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + RotationOffset, 0);
         Vector3 Forward = rotation * Vector3.forward;
         Ray r = new Ray(transform.position, Forward);
