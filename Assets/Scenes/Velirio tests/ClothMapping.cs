42c0db0045b668a6a8ef559b5820973ae61135a8
Velirio <122228321+Velirio@users.noreply.github.com>
2023-02-07 23:40:47 +0100

    First commit / cape script
    Cape mesh needs to be assigned to bones

---
+++ Assets/Scenes/Velirio tests/ClothMapping.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RootMotion.FinalIK;
+
+public class ClothMapping : MonoBehaviour
+{
+    public IK ik;
+
+    [System.Serializable]
+    public class Map
+    {
+        public Transform clothBone;
+        public Transform closestBone;
+
+        private Vector3 relPos;
+        private Quaternion relRot = Quaternion.identity;
+
+        public void Init()
+        {
+            relPos = closestBone.InverseTransformPoint(clothBone.position);
+            relRot = Quaternion.Inverse(closestBone.rotation) * clothBone.rotation;
+        }
+
+        public void Apply()
+        {
+            clothBone.position = closestBone.TransformPoint(relPos);
+            clothBone.rotation = closestBone.rotation * relRot;
+        }
+    }
+
+    public Map[] maps = new Map[0];
+
+    private void Start()
+    {
+        foreach (Map map in maps) map.Init();
+    }
+
+    private void OnEnable()
+    {
+        ik.GetIKSolver().OnPostUpdate += OnPostUpdate;
+    }
+
+    void OnPostUpdate()
+    {
+        foreach (Map map in maps) map.Apply();
+    }
+
+    private void OnDisable()
+    {
+        if (ik != null) ik.GetIKSolver().OnPostUpdate -= OnPostUpdate;
+    }
+}
